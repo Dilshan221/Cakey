73178b608cc1f99d549e3313812f30d911a6fc80
dewdunu chathura <dewdunuc1990@gmail.com>
2025-10-18 18:28:09 +0530

    Delete backend directory

--- backend/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Minimal replacement for express-async-handler
-export default function asyncHandler(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
